fcd92d880b191dcab8a3678e621c5a6c6cd81865
DucHoang <ndhdldt1@gmail.com>
2017-01-01 00:11:49 +0700

    Dont time step 2

--- sunghiepvathoigian/js.js
+++ sunghiepvathoigian/js.js
@@ -33,24 +33,29 @@ Tổng hợp chức năng:
 */
 $(document).ready(function(){
     var time = {
-        timeView: function(total_hours, hours_spent){
+        timeView: function(total_times, times_spent){
             // Vẽ đường biểu diễn time bằng canvas
             // Đơn vị thời gian sử dụng theo giờ
-            // total_hours: Tổng số giờ
-            // hours_spent: Giờ đã dùng
+            // total_times: Tổng số giờ
+            // times_spent: Giờ đã dùng
             var $timeView = $('#time-view'); 
             var canvas = $timeView[0];
 
             // convert giờ sang px: 1h = 1/3px <=> 3h = 1px
-            var total_hours_px = total_hours*(1/3);
-            var hours_spent_px = hours_spent*(1/3);
+            var total_times_px = total_times*(1/3);
+            var times_spent_px = times_spent*(1/3);
 
             // Thuộc tính width of canvas dài thêm 10px để hiển thị đầy đủ
-            var width = total_hours_px + 10 + 'px';
-            $timeView.attr('width',width)
+            var width = total_times_px + 10 + 'px';
+            $timeView.attr('width',width);
 
             // Đường bao biểu diễn time - dài thêm 6px
-            var frame_wrap = total_hours_px + 6;
+            var frame_wrap = total_times_px + 6;
+
+            // Thay đổi giao diện number time 
+            $('span.time .times-total').text(total_times);
+            $('span.time .times-spent').text(times_spent);
+            $('span.time .times-rest').text(total_times - times_spent)
 
             // Vẽ khung bao time
             ctx = canvas.getContext('2d');
@@ -63,11 +68,10 @@ $(document).ready(function(){
             ctx.lineTo(0,16);
             ctx.lineTo(0,0);
             ctx.stroke();
-
             // Vẽ hình chữ nhật biểu diễn thời gian đã dùng
             ctx.fillStyle = "#1c75bb";
-            ctx.fillRect(3,3,hours_spent_px,10);
+            ctx.fillRect(3,3,times_spent_px,10);
         }
     }
-    time.timeView(500, 500);
+    time.timeView(3300, 10);
 });
